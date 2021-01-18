1e4f4bb9570ce290a88a7c951cb0acb6f5040d69
BuckBinbo <htlzkahsy@gmail.com>
2021-01-18 17:32:42 +0800

    init dot

---
+++ dot.cu
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <memory>
+#include "cuda.h"
+bool allclose(float x, float y, float threshold = 1e-4) {
+	return abs(x - y) < threshold;
+}
+	
+void dotc(float*dst, float*src1, float*src2, int xa, int ya, int za, int wa) {
+	//xy * zw -> xz * yw
+	int ywa = ya * wa;
+	for(int xi = 0; xi < xa; xi++) {
+		for(int yi = 0; yi < ya; yi++) {
+			for(int zi = 0; zi < za; zi++) {
+				for(int wi = 0; wi < wa; wi++) {
+					int xzi = xi * za + zi;
+					int ywi = yi * wa + wi;
+					int i = xzi * ywa + ywi;
+					int xyi = xi * ya + yi;
+					int zwi = zi * wa + wi;
+	 				dst[i] = src1[xyi] * src2[zwi];
+	 			}
+			}
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	int xa = argc > 1 ? std::atoi(argv[1]) : 2;
+  int ya = argc > 2 ? std::atoi(argv[2]) : 2;
+	int za = argc > 3 ? std::atoi(argv[3]) : 2;
+  int wa = argc > 4 ? std::atoi(argv[4]) : 2;
+	float* src1 = (float*)malloc(sizeof(float) * xa * ya);
+	float* src2 = (float*)malloc(sizeof(float) * za * wa);
+	float* dst = (float*)malloc(sizeof(float)* xa * ya * za * wa);
+	std::cout<<"------SRC1 / SRC2--------"<<std::endl;
+	for(int xi = 0; xi < xa; xi++) {
+		for(int yi = 0; yi < ya; yi++) {
+			src1[xi * ya + yi] = xi * ya + yi;
+			std::cout<<src1[xi * ya + yi]<<" ";
+		}
+		std::cout<<std::endl;
+	}
+	std::cout<<"+++++THRES+++++++"<<std::endl;
+	  std::cout<<"------SRC1 / SRC2--------"<<std::endl;
+  for(int zi = 0; zi < za; zi++) {
+    for(int wi = 0; wi < wa; wi++) {
+			src2[zi * wa + wi] = zi * wa + wi;
+      std::cout<<src2[zi * wa + wi]<<" ";
+    }
+		std::cout<<std::endl;
+	}
+	std::cout<<"SRC -> DST"<<std::endl;
+	dotc(dst, src1 ,src2, xa, ya, za, wa);
+	std::cout<<"******CPU DST*********"<<std::endl;
+	int xza = xa * za;
+	int ywa = ya * wa;
+	for(int xzi = 0; xzi < xza; xzi++) {
+		for(int ywi = 0; ywi < ywa; ywi++) {
+			std::cout<<dst[xzi * ywa + ywi]<<" ";
+		}
+		std::cout<<std::endl;
+	}
+}
