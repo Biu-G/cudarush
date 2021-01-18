d5a1cc24f59dacb7a82e2ffdea7ed069a8788b89
BuckBinbo <htlzkahsy@gmail.com>
2021-01-18 19:21:55 +0800

    <CPU> GPU VER

--- dot.cu
+++ dot.cu
@@ -4,7 +4,26 @@
 bool allclose(float x, float y, float threshold = 1e-4) {
 	return abs(x - y) < threshold;
 }
-	
+
+__global__ void dotg(float*dst, float*src1, float*src2, int xa, int ya, int za, int wa) {
+  //xy * zw -> xz * yw
+  int ywa = ya * wa;
+  for(int xi = 0; xi < xa; xi++) {
+    for(int yi = 0; yi < ya; yi++) {
+      for(int zi = 0; zi < za; zi++) {
+        for(int wi = 0; wi < wa; wi++) {
+          int xzi = xi * za + zi;
+          int ywi = yi * wa + wi;
+          int i = xzi * ywa + ywi;
+          int xyi = xi * ya + yi;
+          int zwi = zi * wa + wi;
+          dst[i] = src1[xyi] * src2[zwi];
+        }
+      }
+    }
+  }
+}
+
 void dotc(float*dst, float*src1, float*src2, int xa, int ya, int za, int wa) {
 	//xy * zw -> xz * yw
 	int ywa = ya * wa;
@@ -59,7 +78,7 @@ int main(int argc, char* argv[]) {
     }
 		std::cout<<std::endl;
 	}
-	std::cout<<"SRC -> DST"<<std::endl;
+	std::cout<<"SRC ->CPU -> DST"<<std::endl;
 	checkpointc();
 	dotc(dst, src1 ,src2, xa, ya, za, wa);
 	double cstime = checkpointc();
@@ -72,5 +91,18 @@ int main(int argc, char* argv[]) {
 		}
 		std::cout<<std::endl;
 	}*/
+	memset(dst, 0, sizeof(float)* xa * ya * za * wa);
+	dim3 grids, blocks;
+	grids.x = 1;
+	grids.y = 1;
+	grids.z = 1;
+	blocks.x = 1;
+	blocks.y = 1;
+	blocks.z = 1;
+	checkpointc();
+	std::cout<<"SRC -> GPU ->DST"<<std::endl;
+	dotg<<<grids, blocks>>>(dst, src1, src2, xa, ya, za, wa);
+	double dstime = checkpointc();
 	std::cout<<"CSTIME: "<<cstime<<std::endl;
+	std::cout<<"DSTIME: "<<dstime<<std::endl;
 }
